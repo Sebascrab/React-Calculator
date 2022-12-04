0e1889f8d34c91ba5cfc3845fd0928f3cf4aba44
Sebastian Tischner <alexanders.tischner@outlook.com>
2022-12-03 18:35:34 -0700

    buttons active

--- src/App.js
+++ src/App.js
@@ -14,6 +14,11 @@ const App = () => {
     setText((text) => [...text, val + " "]);
   };
 
+  const resetInput = () => {
+    setText("")
+    setResult("")
+  };
+
   const buttonColor = 'orange'
 
 
@@ -22,31 +27,31 @@ const App = () => {
       <div className='calc-wrap'>
           <Input text={text} result={result} />
         <div className='row'>
-          <Button symbol="7" />
-          <Button symbol="8" />
-          <Button symbol="9" />
-          <Button symbol="/" color={buttonColor} />
+          <Button symbol="7" handleClick={addToText} />
+          <Button symbol="8" handleClick={addToText} />
+          <Button symbol="9" handleClick={addToText} />
+          <Button symbol="/" color={buttonColor} handleClick={addToText} />
         </div>
         <div className='row'>
-          <Button symbol="4" />
-          <Button symbol="5" />
-          <Button symbol="6" />
-          <Button symbol="*" color={buttonColor}/>
+          <Button symbol="4" handleClick={addToText} />
+          <Button symbol="5" handleClick={addToText} />
+          <Button symbol="6" handleClick={addToText} />
+          <Button symbol="*" color={buttonColor} handleClick={addToText}/>
         </div>
         <div className='row'>
-          <Button symbol="1" />
-          <Button symbol="2" />
-          <Button symbol="3" />
-          <Button symbol="+" color={buttonColor} />
+          <Button symbol="1" handleClick={addToText} />
+          <Button symbol="2" handleClick={addToText} />
+          <Button symbol="3" handleClick={addToText} />
+          <Button symbol="+" color={buttonColor} handleClick={addToText} />
         </div>
         <div className='row'>
-          <Button symbol="0" />
-          <Button symbol="." />
-          <Button symbol="=" />
-          <Button symbol="-" color={buttonColor}/>
+          <Button symbol="0" handleClick={addToText} />
+          <Button symbol="." handleClick={addToText} />
+          <Button symbol="="  />
+          <Button symbol="-" color={buttonColor} handleClick={addToText} />
         </div>
 
-        <Button symbol="clear" color="red" />
+        <Button symbol="clear" color="red" handleClick={resetInput} />
 
       </div>
     </div>
--- src/components/button.js
+++ src/components/button.js
@@ -2,6 +2,8 @@ import "./button.css"
 
 export const Button = ({ symbol, color, handleClick }) => {
     return (
-        <div className="button-wrap" style={{ backgroundColor: color }}>{symbol}</div>
+        <div onClick={() => handleClick(symbol)}
+            className="button-wrap"
+            style={{ backgroundColor: color }}>{symbol}</div>
     )
 };
